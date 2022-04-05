dbe36e86ef83eb01b0180f891a08b7ea92c71276
Adina Henderson <adinahenderson.mail@gmail.com>
2022-04-05 14:00:03 -0400

    Segregated About from Home page, modified content -- work in progress

---
+++ src/pages/About/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { aboutObj } from "../About/AboutContent";
+import { ContentSection,   } from "../../components";
+
+
+const About = () => {
+  return (
+    <>
+      <ContentSection {...aboutObj} />
+     
+    </>
+  );
+};
+
+export default About;
---
+++ src/pages/About/AboutContent.js
@@ -0,0 +1,38 @@
+// About Content
+export const aboutObj = {
+  lightBg: false,
+  primary: true,
+  imgStart: "",
+  lightTopTitle: true,
+  lightTextDesc: true,
+  buttonLabel: "Go to Projects",
+  description: `
+  During the pandemic, I decided to extend my knowledge in Technolgy. I have always been passionate about programming and technology, therefore I acquired a Coding certificate from Tecnologico de Monterrey. Having an Accounting and Finance background, I worked in the past as an Auditor and Consultant in different areas of global, international and domestic organizations. Having contributed to global projects were business and operational processes were outsourced, aside of Compliance, Process Simplification and Automation was a must.                                                         
+   
+  Often worked with teams using Agile and Scrum methodologies. I have contributed with the creation of dashboards and databases for data analysis and manipulation. Now my focus is to continue to grow as a Full Stack Web Developer.`,
+  headLine: "Adina Henderson",
+  lightText: true,
+  start: "",
+  img: require("../../images/pic-profile.jpg"),
+  alt: "Profile Picture",
+};
+
+// About Content - Spanish
+export const aboutObjSp = {
+  lightBg: false,
+  primary: true,
+  imgStart: "",
+  lightTopTitle: true,
+  lightTextDesc: true,
+  headLine: "Adina Henderson",
+  description: `Durante la pandemia, decidí ampliar mis conocimientos en el área tecnológica. Siempre me había apasionado la programación y tecnología, por lo que adquirí un certificado en el Tecnológico de Monterrey como Full Stack Web Developer.
+  
+  `,
+
+  lightText: true,
+  topTitle: "Full Stack Web Developer",
+  buttonLabel: "Ir a Projectos",
+  start: "",
+  img: require("../../images/pic-profile.jpg"),
+  alt: "Profile Picture",
+};
